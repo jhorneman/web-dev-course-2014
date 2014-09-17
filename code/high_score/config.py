628e0dd93a1d44bdfd01dce261c51ef2873ddedb
Jurie Horneman <jurie@jurie.org>
2014-09-17 13:32:34 +0200

    Removed old high score project which used SQLite.

--- code/high_score/config.py
+++
@@ -1,6 +0,0 @@
-# Configuration values.
-DATABASE = 'high_scores.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
--- code/high_score/schema.sql
+++
@@ -1,6 +0,0 @@
-drop table if exists scores;
-create table scores (
-  id integer primary key autoincrement,
-  player_name text not null,
-  score integer
-);
