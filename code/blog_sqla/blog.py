74bb52391974e45025153bb61d4b68699b88a0d8
Jurie Horneman <jurie@jurie.org>
2014-09-18 13:52:55 +0200

    Added missing view functions and templates.

--- code/blog_sqla/blog.py
+++ code/blog_sqla/blog.py
@@ -23,5 +23,19 @@ def single_post(post_id):
     return render_template('single_post.html', post=post)
 
 
+@app.route('/author/<int:author_id>')
+def author(author_id):
+    author = Author.query.get_or_404(author_id)
+    posts = Post.query.filter(Post.author_id == author_id).all()
+    return render_template('author.html', author=author, posts=posts)
+
+
+@app.route('/category/<int:category_id>')
+def category(category_id):
+    category = Category.query.get_or_404(category_id)
+    posts = Post.query.filter(Post.category_id == category_id).all()
+    return render_template('category.html', category=category, posts=posts)
+
+
 if __name__ == '__main__':
     app.run()
