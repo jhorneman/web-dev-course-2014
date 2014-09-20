71a3728c28081ad4ecc802d933691064a1b4c00a
Jurie Horneman <jurie@jurie.org>
2014-09-20 11:04:38 +0200

    Refactored tests.

--- code/blog_sqla/blog_tests.py
+++ code/blog_sqla/blog_tests.py
@@ -1,48 +1,49 @@
 # coding: utf8
 
 import unittest
-import blog
+from blog import create_app
 from db import create_db
+
+
+app = create_app()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+app.config['TESTING'] = True
 
 
 class BlogTestCase(unittest.TestCase):
     def setUp(self):
-        blog.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-        blog.app.config['TESTING'] = True
-        self.app = blog.app.test_client()
-        create_db()
-
-    def tearDown(self):
-    	pass
+        self.client = app.test_client()
+        with app.app_context():
+            create_db(app.db)
 
     def test_index_page(self):
-        rv = self.app.get('/')
+        rv = self.client.get('/')
         assert rv.data.count("in the category") == 4
 
     def test_single_post_page(self):
-        rv = self.app.get('/post/1')
+        rv = self.client.get('/post/1')
         assert "Problems that aren’t actually solvable" in rv.data
 
     def test_author_page(self):
-        rv = self.app.get('/author/1')
+        rv = self.client.get('/author/1')
         assert rv.data.count("in the category") == 2
         assert "Paris is a huge place" in rv.data
         assert "George Buckingham" in rv.data
 
     def test_category_page(self):
-        rv = self.app.get('/category/1')
+        rv = self.client.get('/category/1')
         assert "Problems that aren’t actually solvable" in rv.data
 
     def test_single_post_404(self):
-        rv = self.app.get('/post/10')
+        rv = self.client.get('/post/10')
         assert rv.status_code == 404
 
     def test_author_404(self):
-        rv = self.app.get('/author/10')
+        rv = self.client.get('/author/10')
         assert rv.status_code == 404
 
     def test_category_404(self):
-        rv = self.app.get('/category/10')
+        rv = self.client.get('/category/10')
         assert rv.status_code == 404
 
 
