61a563539ca7495c68700fef144997849f5b7e3f
Jurie Horneman <jurie@jurie.org>
2014-09-20 11:15:24 +0200

    Exercise.

--- code/blog_sqla/blog_tests.py
+++ code/blog_sqla/blog_tests.py
@@ -24,24 +24,6 @@ class BlogTestCase(unittest.TestCase):
         rv = self.client.get('/post/1')
         assert "Problems that aren’t actually solvable" in rv.data
 
-    def test_author_page(self):
-        rv = self.client.get('/author/1')
-        assert rv.data.count("in the category") == 2
-        assert "Paris is a huge place" in rv.data
-        assert "George Buckingham" in rv.data
-
-    def test_category_page(self):
-        rv = self.client.get('/category/1')
-        assert "Problems that aren’t actually solvable" in rv.data
-
-    def test_single_post_404(self):
-        rv = self.client.get('/post/10')
-        assert rv.status_code == 404
-
-    def test_author_404(self):
-        rv = self.client.get('/author/10')
-        assert rv.status_code == 404
-
     def test_category_404(self):
         rv = self.client.get('/category/10')
         assert rv.status_code == 404
