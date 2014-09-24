551107c4107cc4e3914e57830ab8e99d370f1bbc
Jurie Horneman <jurie@jurie.org>
2014-09-24 10:43:27 +0200

    Tweaks.

--- code/excel_import/excel_import.py
+++ code/excel_import/excel_import.py
@@ -4,9 +4,9 @@ book = open_workbook('blog_data.xls')
 
 sheet = book.sheet_by_index(0)
 
-column_names = sheet.row(0)
+column_names = [c.value for c in sheet.row(0)]
 
 for row_index in range(1, sheet.nrows):
 	row = sheet.row(row_index)
 	for (column_index, column_name) in enumerate(column_names):
-		print column_name, row[column_index].value.encode('ascii', 'replace')
+		print column_name, ':', row[column_index].value.encode('ascii', 'replace')
