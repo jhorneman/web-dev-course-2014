23b03cde764dfe4c72ad5955f949f6c5f754fa4c
Jurie Horneman <jurie@jurie.org>
2014-09-24 09:33:24 +0200

    New exercise.

---
+++ code/excel_import/excel_import.py
@@ -0,0 +1,12 @@
+from xlrd import open_workbook
+
+book = open_workbook('blog_data.xls')
+
+sheet = book.sheet_by_index(0)
+
+column_names = sheet.row(0)
+
+for row_index in range(1, sheet.nrows):
+	row = sheet.row(row_index)
+	for (column_index, column_name) in enumerate(column_names):
+		print column_name, row[column_index].value.encode('ascii', 'replace')
